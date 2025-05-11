66611a3ac5f9b55919b2b58983c1215f5322f91e
Soumadip Dey <soumadipdey63@gmail.com>
2025-05-11 23:31:16 +0530

    simplified kruskal algo implementation

--- 18_graph_kruskal.c
+++ 18_graph_kruskal.c
@@ -1,79 +1,86 @@
 #include "graph.c"
-#define INFINITY __INT_MAX__
 struct Edge {
     char src;
     char dst;
     int weight;
-    int used;
 };
 
-struct Edge* createEdgeList(struct List** adjList, int V, int E, int graph_type) {
-    int edge_index = 0;
-    struct Edge* edgeList = (struct Edge*)malloc(E * sizeof(struct Edge));
-    struct Node* neighbour;
+struct EdgeData {
+    struct Edge* edgeList;
+    int size;
+};
 
+struct EdgeData createSortedEdgeList(struct List** adjList, int V, int E, int graph_type) {
+    //Creating an empty edge list
+    struct Edge* edgeList = NULL;
+    struct EdgeData edgeData;
+    if(graph_type != 0){ 
+        edgeList = (struct Edge*)malloc(E * sizeof(struct Edge));
+        edgeData.size = E;
+        
+    } else { 
+        edgeList = (struct Edge*)malloc(2 * E * sizeof(struct Edge));
+        edgeData.size = 2 * E;
+    }
+    //Populating the empty edge list with edges from the adjacency list
+    struct Node* neighbour;
+    int edge_index = 0;
     for (int i = 0; i < V; i++) {
         neighbour = adjList[i]->head;
         while (neighbour) {
-            //Consider undirected edges only once.
-            if (graph_type == 0 && i + 'A' > neighbour->vertex) {
-                neighbour = neighbour->next;
-                continue;
-            }
-
             edgeList[edge_index].src = i + 'A';
             edgeList[edge_index].dst = neighbour->vertex;
             edgeList[edge_index].weight = neighbour->weight;
-            edgeList[edge_index].used = 0;
             neighbour = neighbour->next;
             edge_index++;
         }
     }
-    return edgeList;
-}
-
-struct Edge findMinValidEdge(struct Edge* edgeList, int visited[], int E, int graph_type) {
-    int src_index, dst_index, min_index = -1;
-    struct Edge min_edge;
-    min_edge.src = min_edge.dst = '\0';
-    min_edge.weight = INFINITY;
-
-    for (int i = 0; i < E; i++) { 
-        src_index = edgeList[i].src - 'A';
-        dst_index = edgeList[i].dst - 'A';
-        if (!edgeList[i].used && (!visited[src_index] || !visited[dst_index])) {
-            if (edgeList[i].weight < min_edge.weight) {
-                min_index = i;
-                min_edge = edgeList[i];
+    //Sorting the edge list based on weights using selection sort
+    for (int i = 0; i < edgeData.size - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < edgeData.size; j++) {
+            if (edgeList[j].weight < edgeList[min_index].weight) {
+                min_index = j;
             }
         }
+        struct Edge temp = edgeList[i];
+        edgeList[i] = edgeList[min_index];
+        edgeList[min_index] = temp;
     }
-
-    if (min_index != -1) {
-        edgeList[min_index].used = 1;
-    }
-
-    return min_edge;
+    edgeData.edgeList = edgeList;
+    return edgeData;
 }
 
 void kruskalMST(struct List** adjList, int V, int E, int graph_type) {
-    int visited[V], edge_count = 0, cost = 0;
-    for (int i = 0; i < V; i++) visited[i] = 0;
-    struct Edge* edgeList = createEdgeList(adjList, V, E, graph_type);
+    int component[V], edge_count = 0, cost = 0 , srcComp, dstComp;
+    for (int i = 0; i < V; i++){ component[i] = i;}
+    // Creating a sorted edge list
+    struct EdgeData edgeData = createSortedEdgeList(adjList, V, E, graph_type);
+    struct Edge* edgeList = edgeData.edgeList;
     struct Edge curr_edge;
-
+    int edge_index = 0;
     while (edge_count < V - 1) {
-        curr_edge = findMinValidEdge(edgeList, visited, E, graph_type);
-        //if (curr_edge.dst == '\0') {
-            //printf("Graph is Disconnected. No MST!\n");
-            //return;
-        //}
-        visited[curr_edge.src - 'A'] = visited[curr_edge.dst - 'A'] = 1;
-        cost += curr_edge.weight;
-        printf("Selected %c ---%d---> %c\n", curr_edge.src, curr_edge.weight, curr_edge.dst);
-        edge_count++;
+        curr_edge = edgeList[edge_index];
+        srcComp = component[curr_edge.src - 'A'];
+        dstComp = component[curr_edge.dst - 'A'];
+        // Check if the current edge does not form a cycle
+        if(srcComp != dstComp){ 
+            cost += curr_edge.weight;
+            printf("Selected %c ---%d---> %c\n", curr_edge.src, curr_edge.weight, curr_edge.dst);
+            // Cascadingly merge the components of the src and dst vertices
+            for (int i = 0; i < V; i++) {
+                if (component[i] == srcComp) {
+                    component[i] = dstComp;
+                }
+            }
+            edge_count++;   
+        }
+        if(edge_index >= edgeData.size) {
+            printf("Graph is Disconnected. No MST!\n");
+            return;
+        }
+        edge_index++;
     }
-
     printf("Cost of the MST is: %d\n", cost);
 }
 
