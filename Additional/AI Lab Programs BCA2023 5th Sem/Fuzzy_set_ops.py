aa9603838fe0e5dea66476f569c8390a4d0368da
soumadipDey <soumadipdey63@gmail.com>
2025-08-18 10:58:33 +0530

    some updates for fuzzy ops

--- Additional/AI Lab Programs BCA2023 5th Sem/Fuzzy_set_ops.py
+++ Additional/AI Lab Programs BCA2023 5th Sem/Fuzzy_set_ops.py
@@ -1,45 +1,64 @@
-# This code works ok for fuzzy sets having the same number of elements.
-# I will implement the general version later.
-
-members = input("Enter the members of the set: ").split()
+import numpy as np
+membersA = input("Enter the members of the set A: ").split()
 deg_memA = list(map(float,input("Enter the degree of membership for A Set: ").split()))
+
+membersB = input("Enter the members of the set B: ").split()
 deg_memB = list(map(float,input("Enter the degree of membership for B Set: ").split()))
 
 setA = {}
 setB = {}
 
-for mem,degA,degB in zip(members, deg_memA, deg_memB):
-    setA[mem] = degA
-    setB[mem] = degB
+for memA,degA in zip(membersA, deg_memA):
+    setA[memA] = degA
+
+for memB,degB in zip(membersB, deg_memB):
+    setB[memB] = degB
 
 print("Set A:\n",setA)
 print("\nSet B:\n",setB)
 
 def fuzzyUnion(setA:dict, setB:dict):
     setAUB = {}
-    for mem in setA.keys():
-        setAUB[mem] = max(setA[mem], setB[mem])
-
+    all_members = set(setA.keys()).union(setB.keys())
+    for mem in all_members:
+        setAUB[mem] = max(setA.get(mem,0.0), setB.get(mem,0.0))
     return setAUB
 
 def fuzzyIntersection(setA:dict, setB:dict):
     setAandB = {}
-    for mem in setA.keys():
-        setAandB[mem] = min(setA[mem], setB[mem])
-
+    all_members = set(setA.keys()).union(setB.keys())
+    for mem in all_members:
+        setAandB[mem] = min(setA.get(mem,0.0), setB.get(mem,0.0))
     return setAandB
 
-def fuzzyComplement(setA):
+def fuzzyComplement(setA:dict):
     setAcomplement = {mem: round(1.0 - setA[mem],2) for mem in setA.keys()}
     return setAcomplement
 
-def fuzzyAlgebraicSum(setA, setB):
-    setAplusB = {mem: round(setA[mem] + setB[mem] - setA[mem]*setB[mem],2) for mem in setA.keys()}
+def fuzzyAlgebraicSum(setA:dict, setB:dict):
+    setAplusB = {}
+    all_members = set(setA.keys()).union(setB.keys())
+    for mem in all_members:
+        setAplusB[mem] = round(
+            setA.get(mem,0.0) + 
+            setB.get(mem,0.0) - 
+            setA.get(mem,0.0) * setB.get(mem,0.0),2)
     return setAplusB
 
-def fuzzyAlgebraicProduct(setA, setB):
-    setAdotB = {mem: round(setA[mem] * setB[mem],2) for mem in setA.keys()}
+def fuzzyAlgebraicProduct(setA:dict, setB:dict):
+    setAdotB = {}
+    all_members = set(setA.keys()).union(setB.keys())
+    for mem in all_members:
+            setAdotB[mem] = round(setA.get(mem,0.0) * setB.get(mem,0.0),2)
     return setAdotB
+
+def fuzzyCartesianProduct(setA:dict, setB:dict):
+    cp_matrix = np.zeros((len(setA),len(setB)))
+    for i, memA in enumerate(setA.keys()):
+        for j, memB in enumerate(setB.keys()):
+            cp_matrix[i][j] = min(setA[memA],setB[memB])
+
+    return cp_matrix
 
 print("\nA Union B:\n",fuzzyUnion(setA,setB))
 print("\nA Intersection B:\n",fuzzyIntersection(setA,setB))
@@ -47,3 +66,4 @@ print("\nA Complement:\n",fuzzyComplement(setA))
 print("\nB Complement:\n",fuzzyComplement(setB))
 print("\nAlgebriac Sum of A & B:\n",fuzzyAlgebraicSum(setA,setB))
 print("\nAlgebriac Product of A & B:\n",fuzzyAlgebraicProduct(setA,setB))
+print("\nCartesian Product of Fuzzy Sets A and B: \n",fuzzyCartesianProduct(setA, setB))
